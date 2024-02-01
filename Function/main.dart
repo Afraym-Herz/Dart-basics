663b195784d2327399c11e054333c1441be6be0f
Afraym-Herz <113017458+Afraym-Herz@users.noreply.github.com>
2024-02-01 23:48:55 +0200

    Add files via upload

---
+++ Function/main.dart
@@ -0,0 +1,32 @@
+int num ()=>2 ;
+
+
+Function prword() {
+  print ("word 1") ;
+  return (){
+    print ("Anonymous 2") ; 
+    return (){
+      print ("Anonymous 3") ;  };
+  };
+
+}
+
+Function prwordt (){
+  print ("word 2") ;
+  return (String word)
+  {print ("Anonymous 4 $word") ;} ;
+}
+
+Function renum (){
+  return num ; // to save value of function renum but not execute 
+}
+
+void main (){
+  prword()()() ;
+
+  var saveFunction = renum ;
+
+  prword() ;
+      
+  prwordt()("Afryam") ;
+}
