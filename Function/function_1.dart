c5b4624e5ee3875017e3d027c9c1ee0e2029d96f
Afraym-Herz <113017458+Afraym-Herz@users.noreply.github.com>
2024-02-01 23:31:36 +0200

    Update and rename function_1 to function_1.dart

---
+++ Function/function_1.dart
@@ -0,0 +1,8 @@
+
+void prwo ({required String w}){ // required keyword make us must write parameter's name when we call fuction and set the arguments
+  print (w) ;
+}
+
+void main (){
+prwo(w:"d") ;
+}
