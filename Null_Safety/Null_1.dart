b756b83600edd8521c9111c544bfab3679efd8c6
Afraym-Herz <113017458+Afraym-Herz@users.noreply.github.com>
2024-02-02 12:29:40 +0200

    Update Null_1.dart

--- Null_Safety/Null_1.dart
+++ Null_Safety/Null_1.dart
@@ -1,18 +1,22 @@
-void makeCoffee (  String coffee ,  [ String? dairy , String? dount]){
-    if (dairy!=null&& dount !=null  ){
-      print ("$coffee with $dairy and $dount") ;
+void makeCoffee ( {required String coffee, String? dairy, String donut = "out donut"}){
+    if (dairy!=null&& donut !=null  ){
+      print ("$coffee with $dairy and $donut") ;
     }
-    else if (dairy!=null&& dount == null){
+    else if (dairy!=null&& donut == null){
       print ("$coffee with $dairy") ;
     }
-    else if (dairy==null && dount!=null){
-      print ("$coffee with only $dount") ;
+    else if (dairy==null && donut!=null){
+      print ("$coffee with only $donut") ;
     }
-    else print ("black $coffee") ;
+    else print ("black $coffee");
+
+    
+   
 }
 
 
 void main (){
-  makeCoffee( "franch","milk") ;
-makeCoffee( "franch",null,"ice") ;
+  makeCoffee(coffee: "chocolatey") ;
+    makeCoffee(coffee: "chocolatey",dairy: "milk") ;
+      makeCoffee( coffee: "chocolatey", donut: "chockolate donut") ;
 }
