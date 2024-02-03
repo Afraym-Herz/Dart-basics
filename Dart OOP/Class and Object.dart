cf5d8a26254ef9aeb5d76631c9cfb0ce004fd3da
Afraym-Herz <113017458+Afraym-Herz@users.noreply.github.com>
2024-02-03 20:28:01 +0200

    Update and rename Class and Object to Class and Object.dart

---
+++ Dart OOP/Class and Object.dart
@@ -0,0 +1,35 @@
+
+void main() {
+Human h = new Human() ;
+h.name = 'Afraym' ;
+h.age = 19 ;
+h.height = 182 ;
+
+    print (h.name) ;
+    print (h.age) ;
+    print (h.height) ;
+    print ("-----------------------") ;
+    h.info() ;
+
+Human h = new Human() ;
+h.name = 'Afraym' ;
+h.age = 19 ;
+h.height = 182 ;
+
+    print (h.name) ;
+    print (h.age) ;
+    print (h.height) ;
+    print ("-----------------------") ;
+    h.info() ;
+}   
+
+    
+class Human {
+  var name , age , height ;
+  void info (){
+    print ("the Name is : $name") ;
+    print ("the age is : $age") ;
+    print ("the height is : $height") ;
+  }
+
+}
