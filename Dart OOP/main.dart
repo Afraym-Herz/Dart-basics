9d44c98eb3b4e6836b8b9989d7b3b08c43f89098
Afraym-Herz <113017458+Afraym-Herz@users.noreply.github.com>
2024-02-05 22:39:24 +0200

    Add files via upload

---
+++ Dart OOP/main.dart
@@ -0,0 +1,52 @@
+/// import Human ;
+void main() {
+
+    Lion scar = Lion () ;
+    Dog jack = Dog();
+    Cat kitty = Cat(); 
+
+
+    scar.eat();
+    jack.eat();
+    kitty.eat();
+
+    scar.roar();
+    jack.bark();
+    kitty.meo();
+}   
+
+
+   class Animal {
+    int _numOfLegs = 4 ;
+    void eat (){
+      print ("eating") ;
+      }
+
+    void sleep(){}
+    void walk(){}
+
+   }
+
+
+   class Dog extends Animal {
+    void bark(){
+      print ("barking") ;
+    }
+
+   }
+   
+
+   class Cat extends Animal {
+    void meo(){
+      print ("meo") ;
+    }
+    
+   }
+
+
+   class Lion extends Animal {
+    void roar(){
+      print ("roaring") ;
+    }
+    
+   }
