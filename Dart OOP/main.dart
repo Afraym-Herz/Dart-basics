dfc623bc5726790c11ebe54a554de0ebfa21694f
Afraym-Herz <113017458+Afraym-Herz@users.noreply.github.com>
2024-02-08 13:35:11 +0200

    Add files via upload

---
+++ Dart OOP/main.dart
@@ -0,0 +1,26 @@
+void main() {
+
+  Circle c1 = Circle(radious: 5, x: 2, y: 1) ;
+  c1.draw() ;
+
+  Circle c2 = Circle.origin(radious: 4) ;
+  c2.draw() ;
+}   
+
+
+
+class Circle {
+  late double radious ; /// late keyword use to tell to compiler we put value in varaibles later
+  late int x , y ;
+
+  Circle({required this.radious  , required this.x , required this.y}) ;
+
+  void draw (){
+    print ("draw circle with radious = $radious , x = $x  and y = $y") ;
+   }
+   Circle.origin ({required this.radious}){ 
+    x = 0 ;
+    y = 0 ;
+   }
+}
+   
