3ccd44012e5f0dc57cd5214f21912a2015abd044
Afraym-Herz <113017458+Afraym-Herz@users.noreply.github.com>
2024-02-09 13:44:42 +0200

    Update Exceptions1.dart

--- Exceptions/Exceptions1.dart
+++ Exceptions/Exceptions1.dart
@@ -1,1 +1,12 @@
+void main() {
+List <String> names = ['Afraym' , 'Herz' , 'Zaki'] ;
+ //print (names[3]) ; // runtime error (exception) because  index 3 doesn't exist
 
+  try {
+    print (names[3]) ;
+  } catch(e) {
+    print (e) ;
+  }
+
+
+}
